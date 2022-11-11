ed2b7ae7700cb607c1f1fded0b02d0dde9a04668
kewenlok <kewen.lok@u.nus.edu>
2022-11-12 00:19:43 +0800

    Add cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const apiRoutes = require("./routes/api");
 const bodyParser = require('body-parser');
+const cors = require('cors')
 require("./config/database").connect();
 
 const app = express();
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
 
 // Use Api routes in the App
 app.use('/api', apiRoutes);
